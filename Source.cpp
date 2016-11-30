13fec1ccfe4f9d9d1aad9d082a7f09676f427acb
Bella Luk <youisme610@gmail.com>
2016-11-29 20:58:02 -0800

    changed the source file
    it now tests our new files

--- Source.cpp
+++ Source.cpp
@@ -5,9 +5,11 @@ using namespace std;
 void main(){
 	Deck myDeck;
 	myDeck.makeDeck();
-	cout << myDeck << endl;
+	//cout << myDeck << endl;
 	myDeck.randomizeDeck();
 	cout << myDeck << endl;
+	Deck newDeck = myDeck.drawDeck(10);
+	cout << newDeck;
 	system("pause");
 
 }
